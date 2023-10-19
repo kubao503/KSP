6423ed74f4b09a1b327c4415007dbe15b9783945
kubao503 <o503@interia.eu>
2023-10-19 13:35:25 +0200

    Better plot drawing

--- starship_sim/pid_log.py
+++ starship_sim/pid_log.py
@@ -18,9 +18,18 @@ class PidLog():
         return [sum(output) for output in zip(self.p_outputs, self.i_outputs, self.d_outputs)]
 
     def plot(self):
-        plt.plot(self.errors, color='black')
-        plt.plot(self.p_outputs, color='r')
-        plt.plot(self.i_outputs, color='g')
-        plt.plot(self.d_outputs, color='b')
-        plt.plot(self.__get_output_sum(), color='grey')
+        fig, ax1 = plt.subplots()
+        ax1.plot(self.errors, color='black')
+        ax1.axhline(0, color='black')
+        ax1.set_ylabel('error')
+
+        ax2 = ax1.twinx()
+        ax2.plot(self.__get_output_sum(), color='grey')
+        ax2.axhline(0, color='grey')
+        ax2.plot(self.p_outputs, color='r')
+        ax2.plot(self.i_outputs, color='g')
+        ax2.plot(self.d_outputs, color='b')
+        ax2.set_ylabel('output')
+
+        fig.tight_layout()
         plt.show()
